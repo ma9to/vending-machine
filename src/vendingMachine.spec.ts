a37d25797e80f7400699adf124cb3251af853991
Masahiro Kumatani <masahiro.kumatani@bizreach.co.jp>
2019-01-08 14:32:01 +0900

    2018-12-18 tdd traning

---
+++ src/vendingMachine.spec.ts
@@ -0,0 +1,52 @@
+// 飲み物自動販売機
+// - 飲み物を選んで購入できる
+// - お金を入れると、入金額が見られる
+// - お金が足りないときは、購入できない
+// - 連続で入れると、入金額が加算される
+// - 飲み物の在庫を追加できる
+// - 飲み物の在庫を確認できる
+// - 使えないお金がある(1,5,2000,5000,10000)
+
+// given: [x]自動販売機の一覧にコーラがある
+// コーラの値段(100円)が決まっている
+// コーラを買うお金を持っている
+// コーラの在庫がある
+// when: 100円をいれる
+//   - 100円入ったよね確認
+//   - 100円で購入できる飲み物がわかる
+// コーラを選択する
+// then: コーラがでてくる
+
+import { VendingMachine } from "./vendingMachine";
+
+fdescribe('飲み物自動販売機', () => {
+  describe('コーラを購入する', () => {
+    it('自動販売機が存在する', () => {
+      const vendingMachine = new VendingMachine();
+      expect(vendingMachine).toBeDefined();
+    });
+    it('自動販売機の一覧にコーラがある', () => {
+        const vendingMachine = new VendingMachine();
+        const bool = vendingMachine.drinkList.some(drink => drink.name === 'cola');
+        expect(bool).toBeTruthy();
+    });
+    describe('100円をいれる', () => {
+        it ('100円入ったよね確認', () => {
+            const vendingMachine = new VendingMachine();
+            expect(vendingMachine.postMoney(100)).toEqual(vendingMachine.getMoney());
+        });
+    });
+  });
+});
+
+// [
+//     {
+//         name: 'cora',
+//         count: 1,
+//         price: 100,
+//     }
+// ]
+// hoge(100);
+// public hoge(money: number): any {
+
+// }
---
+++ src/vendingMachine.ts
@@ -0,0 +1,9 @@
+export class VendingMachine {
+    drinkList = [
+        {
+            name: 'cola',
+            count: 1,
+            price: 100,
+        }
+    ]
+}
